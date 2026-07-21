fa93d095acfddea3afd5e1d0055955ccd406b377
Royal Shrestha <royalshrestha003@gmail.com>
2026-07-21 23:17:28 +0545

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+
+def register(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        return redirect("login")
+
+    return render(request, "accounts/register.html")
+
+
+def user_login(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+
+            login(request, user)
+
+            return redirect("home")
+
+    return render(request, "accounts/login.html")
+
+
+def user_logout(request):
+
+    logout(request)
+
+    return redirect("home")
---
+++ products/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+
+    return render(request, 'products/products.html', {
+        'products': products
+    })
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+
+    return render(request, 'products/product_detail.html', {
+        'product': product
+    })
