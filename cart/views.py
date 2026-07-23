6b76cfcc98d6efc71ec9c1f7bcc8d6e1e66bb044
Royal Shrestha <royalshrestha003@gmail.com>
2026-07-23 22:17:51 +0545

    Implemented shopping cart models and add to cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Cart, CartItem
+from products.models import Product
+
+
+@login_required
+def add_to_cart(request, product_id):
+
+    product = get_object_or_404(Product, id=product_id)
+
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    cart_item, item_created = CartItem.objects.get_or_create(
+        cart=cart,
+        product=product
+    )
+
+    if not item_created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return redirect("cart")
+
+
+@login_required
+def cart(request):
+
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    items = CartItem.objects.filter(cart=cart)
+
+    total = 0
+
+    for item in items:
+        total += item.subtotal()
+
+    context = {
+        "items": items,
+        "total": total,
+    }
+
+    return render(request, "cart/cart.html", context)
+
+@login_required
+def increase_quantity(request, item_id):
+
+    item = get_object_or_404(CartItem, id=item_id)
+
+    item.quantity += 1
+
+    item.save()
+
+    return redirect("cart")
+
+
+@login_required
+def decrease_quantity(request, item_id):
+
+    item = get_object_or_404(CartItem, id=item_id)
+
+    if item.quantity > 1:
+
+        item.quantity -= 1
+
+        item.save()
+
+    else:
+
+        item.delete()
+
+    return redirect("cart")
+
+
+@login_required
+def remove_item(request, item_id):
+
+    item = get_object_or_404(CartItem, id=item_id)
+
+    item.delete()
+
+    return redirect("cart")
--- royalnepal/urls.py
+++ royalnepal/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
 
     path("accounts/", include("accounts.urls")),
 
+    path("cart/", include("cart.urls")),
+
 ]
 
 if settings.DEBUG:
