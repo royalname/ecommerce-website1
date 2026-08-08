7a2e0709b16a3a0f9cf6e8f2b4daf8600219d2a4
Royal Shrestha <royalshrestha003@gmail.com>
2026-08-08 22:58:49 +0545

    Improve order admin and Kathmandu time display

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.utils import timezone
 from .models import Order, OrderItem
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "user", "full_name", "total_amount", "status", "payment_method", "payment_status", "stock_reduced", "created_at",)
+    list_filter = ("status", "payment_method", "payment_status", "stock_reduced", "created_at",)
+    search_fields = ("user__username", "full_name", "phone", "email",)
+    readonly_fields = ("created_at",)
+    inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "order", "product", "quantity", "price",)
+    list_filter = ("order", "product",)
+    search_fields = ("order__id", "product__name",)
+
